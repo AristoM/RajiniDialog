b1936489c810dcfec1595278aec7069267853350
AristoM <aristo.mca@gmail.com>
2023-09-06 22:30:12 +0530

    RajiniDialogue #2 - New background images added. And instead of picking random song taking next song.

--- app/src/main/java/com/aristom/rajinidialogue/MainActivity.kt
+++ app/src/main/java/com/aristom/rajinidialogue/MainActivity.kt
@@ -1,32 +1,76 @@
 package com.aristom.rajinidialogue
 
+import android.graphics.drawable.Drawable
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Vibrator
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     private var mediaPlayer: MediaPlayer? = null
     private lateinit var playButton: Button
-    private val musicFiles = arrayOf(R.raw.song1, R.raw.song2, R.raw.song3,
-        R.raw.song4, R.raw.song5, R.raw.song6, R.raw.song7) // Add your music file references here
+    private lateinit var stopButton: Button
+    private lateinit var root: LinearLayout
+    private val musicFiles = arrayOf(
+        R.raw.song1,
+        R.raw.song2,
+        R.raw.song3,
+        R.raw.song4,
+        R.raw.song5,
+        R.raw.song6,
+        R.raw.song7
+    ) // Add your music file references here
     private var currentMusicIndex = -1
     private var isReleased = false
     private lateinit var vibrator: Vibrator
-
+    private val backgroundImages = listOf(
+        R.drawable.bg_1,
+        R.drawable.bg_2,
+        R.drawable.bg_3,
+        R.drawable.bg_4,
+        R.drawable.bg_5,
+        R.drawable.bg_6,
+        R.drawable.bg_7
+    ) // Add your image resource IDs here
+    private var currentImageIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        root = findViewById(R.id.root_layout)
         playButton = findViewById(R.id.playButton)
+        stopButton = findViewById(R.id.stopButton)
         mediaPlayer = MediaPlayer()
         vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator // Initialize Vibrator
 
 
         playButton.setOnClickListener { playRandomMusic() }
+        stopButton.setOnClickListener { stopMusic() }
+    }
+
+    private fun stopMusic() {
+        if (mediaPlayer != null && !isReleased && mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.stop()
+            mediaPlayer!!.reset()
+        }
+    }
+
+    private fun changeBackground() {
+        if (currentImageIndex < backgroundImages.size) {
+            val drawable: Drawable? =
+                resources.getDrawable(backgroundImages[currentImageIndex], theme)
+            root.background = drawable
+            currentImageIndex++
+
+            // Reset the index to loop through images
+            if (currentImageIndex >= backgroundImages.size) {
+                currentImageIndex = 0
+            }
+        }
     }
 
     private fun playRandomMusic() {
@@ -36,16 +80,18 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer!!.reset()
         }
 
-        val randomIndex = Random().nextInt(musicFiles.size)
-        if (randomIndex != currentMusicIndex) {
-            currentMusicIndex = randomIndex
-
-            val randomMusicId = musicFiles[currentMusicIndex]
-
-            mediaPlayer = MediaPlayer.create(this, randomMusicId)
-            mediaPlayer!!.start()
-            isReleased = false
+        currentMusicIndex++
+        if (currentMusicIndex >= musicFiles.size) {
+            currentMusicIndex = 0 // Loop back to the first song
         }
+
+        val nextMusicId = musicFiles[currentMusicIndex]
+
+        mediaPlayer = MediaPlayer.create(this, nextMusicId)
+        mediaPlayer!!.start()
+        isReleased = false
+
+        changeBackground()
     }
 
     override fun onStop() {
